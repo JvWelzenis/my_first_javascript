ee3ce5efe3890753fe9ed74fbdb41619ef7ce2ac
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-11-12 13:58:33 +0100

    Add files via upload

---
+++ my_first_javascript/index.js
@@ -0,0 +1,23 @@
+console.log("hello winc academy");
+    let name = 'Jeske';
+    console.log(name);
+
+    // you can't use if becouse it is a reserved keyword
+
+//  let x = 4;
+//  let y = 4;
+//  let z = x + y;
+//  console.log(z); //answer is 8
+
+//  let x = '4';
+//  let y = '4';
+//  let z = x + y;
+//  conlole.log(z); //answer is 44
+
+
+//  var age = 33;
+//  console.log(typeof age);
+
+var age = '33';
+console.log(typeof age);
+    
